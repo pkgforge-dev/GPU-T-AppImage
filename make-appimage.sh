f9ddf0f73245051d9ad84cf254c22fed0efbb183
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-02-15 15:35:03 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,23 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q gpu-t-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/512x512/apps/gpu-t.png
+export DESKTOP=/usr/share/applications/gpu-t.desktop
+export DEPLOY_DOTNET=1
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+mkdir -p ./AppDir/bin
+echo '#!/bin/sh
+exec dotnet "$APPDIR"/lib/GPU-T/GPU-T.dll' > ./AppDir/bin/gpu-t
+chmod +x ./AppDir/bin/gpu-t
+
+quick-sharun /usr/lib/GPU-T /usr/bin/vulkan-info
 
 # Additional changes can be done in between here
 
